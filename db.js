3fa3fef97d89c7e0b8293dbd0da91db42a670b01
ehmaddd <ehmaddd@gmail.com>
2024-05-08 23:59:11 +0500

    Create mongo client

--- db.js
+++ db.js
@@ -2,3 +2,6 @@ const { MongoClient } = require('mongodb');
 
 // MongoDB connection URI
 const uri = 'mongodb://localhost:27017/mydatabase';
+
+// Create a MongoDB client
+const client = new MongoClient(uri);
